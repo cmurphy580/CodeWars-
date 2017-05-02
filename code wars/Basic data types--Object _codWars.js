f8ee69dd0fdab9e0aa98dce29f5935e7307ab1d6
Conor Murphy <cmurphy580@gmail.com>
2017-05-02 16:21:21 -0700

    Add files via upload

---
+++ code wars/Basic data types--Object _codWars.js
@@ -0,0 +1,13 @@
+function animal(obj){
+  return 'This ' + obj.color + ' ' + obj.name + ' has ' + obj.legs + ' legs.';  
+}
+
+/*
+
+BEST PRACTICE: 
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+*/
